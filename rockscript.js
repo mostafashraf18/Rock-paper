004aba9e95295f7051fab9baade7de4436cd548d
mostafashraf18 <ma2963629@gmail.com>
2023-06-08 11:18:47 +0300

    Gui

--- rockscript.js
+++ rockscript.js
@@ -1,91 +1,47 @@
+
+let choices = ['Paper', 'Scissors', 'Rock'];
 let playerScore = 0;
 let computerScore = 0;
 
 
-function getComputerChoice(){
-    const array = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * array.length);
-    const computerGuess = array[randomIndex];
-    return computerGuess;
+const message = document.querySelector('#Message');
+const score = document.querySelector('#Score');
+
+let computerPlay = () => {
+    return choices[Math.floor(Math.random() * choices.length)];
+};
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    if (winGame(playerSelection, computerSelection)) {
+        ++playerScore;
+    }
+    else if (loseGame(playerSelection, computerSelection)) {
+        ++computerScore;
+    }
+    score.textContent = `Current Score: ${playerScore};Computer Score: ${computerScore}`;
+    message.textContent = (winMessage()) ? winMessage():
+    winGame(playerSelection, computerSelection) || loseGame(playerSelection, computerSelection) ||"It's a tie!"; 
+    
 }
 
-function playRound(playerSelection, ComputerSelection){
-    let log = '';
-    if(playerSelection ==='Rock'){
-        if(ComputerSelection === 'Paper'){
-            log = 'You Lose! Paper beats Rock';
-        } else if(ComputerSelection === 'Scissors'){
-            log = 'You Win! Rock beats Scissors';
-        }else{
-            log = "It's a tie";
-        }
-    } else if (playerSelection === 'Paper'){
-        if(ComputerSelection === 'Scissors'){
-            log = 'You Lose! Scissors beats Paper';
-        } else if(ComputerSelection === 'Rock'){
-            log = 'You Win! Paper beats Rock';
-        } else {
-            log = "It's  a  tie";
-        }
-    } else if(playerSelection === 'Scissors'){
-        if (ComputerSelection === 'Rock'){
-            log = 'You Lose! Rock beats Scissors';
-        } else if(ComputerSelection === 'Paper'){
-            log = 'You Win! Scissors beats Paper';
-        } else {
-            log = "It's a tie";
-        }
-    }
-
-    return log;
+function winGame(playerSelection, computerSelection) {
+    return results = playerSelection == 'PAPER' && computerSelection == 'ROCK' ? 'You Win! Paper beats Rock':
+    playerSelection == 'ROCK' && computerSelection == 'SCISSORS' ? 'You Win! Rock beats Scissors':
+    playerSelection == 'SCISSORS' && computerSelection == 'PAPER' ? 'You Win! Scissors beats Paper':
+    false;
 }
 
-function game(){
+function loseGame(playerSelection, computerSelection) {
+    return results = playerSelection == 'PAPER' && computerSelection == 'SCISSORS' ? 'You Lose! Scissors beats Paper':
+    playerSelection == 'ROCK' && computerSelection == 'PAPER' ? 'You Lose! Paper beats Rock':
+    playerSelection == 'SCISSORS' && computerSelection == 'ROCK' ? 'You Lose! Rock beats Scissors':
+    false;
+}
 
-    for(let i = 0; i < 5; i++) {
-
-
-    
-        const playerSelect = window.prompt("Enter you choice.")
-        let playerSelection = capitalize(playerSelect);
-        let computerSelection = getComputerChoice();
-
-        console.log("The computer chose: " + computerSelection);
-        console.log("You chose: " + playerSelection);
-
-        let roundResult = playRound(playerSelection, computerSelection);
-
-        if(roundResult.search('You Win!') > -1){
-            console.log(roundResult);
-            playerScore++;
-        } else if(roundResult.search('You Lose!') > -1){
-            console.log(roundResult);
-            computerScore++;
-        } else if (roundResult.search("It's a tie") > -1 ) {
-            console.log(roundResult);
-        }
-    
-
-        if (playerScore >= 5 && computerScore < 5) {
-        let message = 'Game Over. You Win!';
-        console.log(message);
-        } else if (playerScore < 5 && computerScore >= 5) {
-            message = 'Game Over. You Lose!';
-            console.log(message);
-      }
-    }
-
- }
-
-
-
-
-function capitalize(string) {
-    return (
-      string.toLowerCase().charAt(0).toUpperCase() + string.toLowerCase().slice(1)
-    );
-  }
-
-  game();
+function winMessage() {
+    return (playerScore == 5) ? 'You Win': (computerScore == 5) ? 'Computer Wins':false;
+}
 
   
